0b95cdf455ea1973e8758566d1ec0ccd2afe3666
cxcarvaj <cxcarvaj@gmail.com>
2025-05-19 13:53:13 -0500

    Initial Commit

---
+++ Taskeando/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Taskeando
+//
+//  Created by Carlos Xavier Carvajal Villegas on 19/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Taskeando/TaskeandoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskeandoApp.swift
+//  Taskeando
+//
+//  Created by Carlos Xavier Carvajal Villegas on 19/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskeandoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
